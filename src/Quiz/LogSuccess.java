06a6f8ced737951c7a1e277564cb3d08c3f78031
Kesha Torriana <keshatorriana.work@gmail.com>
2025-06-03 14:24:17 +0800

    app: logSuccess

---
+++ src/Quiz/LogSuccess.java
@@ -0,0 +1,75 @@
+package Quiz;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class LogSuccess extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+    private String username;
+
+	/**
+	 * Launch the application.
+	 */
+	/**
+	 * Create the frame.
+	 * @param username 
+	 */
+	public LogSuccess(String username) {
+		this.username = username; // Store the username
+		setTitle("KidsQuest");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+       
+        //BACKGROUND IMAGE
+        JPanel contentPane = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Image backgroundImage = new ImageIcon("Resources\\logSuccBG.png").getImage();
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                g.drawImage(backgroundImage, 0, 0, screenSize.width, screenSize.height, this);
+            }
+        };
+
+        // Optional: Remove any border around the image
+        contentPane.setBorder(BorderFactory.createEmptyBorder());
+
+        // Set the content pane
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        
+        JButton btnProcc = new JButton("");
+        btnProcc.setIcon(new ImageIcon("Resources\\btnProceed.png"));
+        btnProcc.setFocusPainted(false);
+        btnProcc.setContentAreaFilled(false);
+        btnProcc.setBorderPainted(false);
+        btnProcc.setBounds(513, 470, 475, 159); // Position the Register button
+        contentPane.add(btnProcc);
+        btnProcc.setVisible(true);
+        
+        btnProcc.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		// Redirect to the Category frame
+                LearningMaterial learnFrame = new LearningMaterial(username);
+                learnFrame.setVisible(true);
+                dispose(); // Close the current frame
+            }
+        });
+	}
+
+}
